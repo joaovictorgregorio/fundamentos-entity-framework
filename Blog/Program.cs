4c64875b40e9840daa8c7d4bd362dc1be70eb8b3
João Silva <joaogregoriodsilva@gmail.com>
2025-07-31 15:09:23 -0300

    Início do módulo Operações Básicas, atualizando o repositório com os avanços nos estudos até o momento.

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -5,39 +5,5 @@ using System;
 
 using (var context = new BlogDataContext())
 {
-
-    // var tag = new Tag { Name = "Git", Slug = "git" };
-    // context.Tags.Add(tag);
-
-    // var tag2 = new Tag { Name = "EF", Slug = "entity framework" };
-    // context.Tags.Add(tag2);
-
-    // var tag3 = new Tag { Name = "CSS", Slug = "estilo" };
-    // context.Tags.Add(tag3);
-
-    // context.SaveChanges(); 
-    // System.Console.WriteLine("Tags adicionadas com sucesso!");
-
-    // var tag = context.Tags.FirstOrDefault(x => x.Id == 4);
-    // tag.Name = ".NET";
-    // tag.Slug = "dotnet";
-
-    // context.Update(tag);
-    // context.SaveChanges();
-    // Console.WriteLine("Atualização da TAG feita com sucesso!!!"); 
-
-    // var tag = context.Tags.FirstOrDefault(x => x.Id == 3);
-
-    // context.Remove(tag);
-    // context.SaveChanges(); */
-
-    var tags = context
-        .Tags
-        .AsNoTracking()
-        .ToList();
-
-    foreach (var tag in tags)
-    {
-        Console.WriteLine($"{tag.Name} - {tag.Slug}");
-    }
+    
 }
